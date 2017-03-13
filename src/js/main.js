e34bec67ecb6fc90cf280bfaf7fb53f45fddc3d7
Yaroslav Sydoryk <slaviksydoryk@gmail.com>
2017-03-13 14:37:53 +0200

    header  done

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,25 @@ orderNow.addEventListener("submit", function (e) {
 });
 
 $(function () { // jquery bar rating
+  // collapsed icon menu
+  $('#menu-collapse').on('shown.bs.collapse', function () {
+       $(".glyphicon").removeClass("glyphicon glyphicon-menu-hamburger").addClass("glyphicon glyphicon-menu-up");
+    });
+
+    $('#menu-collapse').on('hidden.bs.collapse', function () {
+       $(".glyphicon").removeClass("glyphicon glyphicon-menu-up").addClass("glyphicon glyphicon-menu-hamburger");
+    });
+    // плавний скролінг на якорі
+  $(document).on('click', 'a', function(e){
+    e.preventDefault();
+    
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
   $('.rate').barrating({
     theme: 'fontawesome-stars',
     allowEmpty: true
-    
   });
 
   $('.journalSection__cardsContainer').masonry({
@@ -26,7 +41,8 @@ $(function () { // jquery bar rating
      freeScroll : true, 
      contain : true, 
      prevNextButtons : false, 
-     pageDots : false 
+     pageDots : false,
+     wrapAround: true
 });
 
 })
